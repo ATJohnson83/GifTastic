9e10a1e2005bc05bd2bc4ffeb6dafa57c0a30c0e
Austin Johnson <ATJohnson83@Gmail.com>
2017-07-30 20:00:12 -0400

    all js except animation on off working, only working on first image in div

---
+++ assets/javascript/app.js
@@ -0,0 +1,71 @@
+var favfoods = ["Fried Catfish", "Pickles", "Chana Masala", "Shrimp Poboy", "Pho"]
+renderButtons();
+getGif();
+
+function getGif(){
+	$('button').on('click', function(){
+		$('#food-display').empty();
+		var food = $(this).attr('data-name');
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    food + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $.ajax({
+    	url: queryURL,
+    	method: "GET"
+    })
+    .done(function(response){
+    	console.log(queryURL);
+    	var results = response.data;
+    	
+    	for (var i = 0; i < results.length; i++) {
+    		var foodDiv = $('<div>');
+    		foodDiv.addClass('foodcontainer');
+    		var p = $('<p>').text('Rating: ' + results[i].rating);
+    		var foodImage = $('<img>');
+    		var animated = results[i].images.fixed_height.url;
+    		var still = results[i].images.fixed_height_still.url;
+				foodImage.attr('src',still);
+				foodDiv.append(p);
+				foodDiv.append(foodImage);
+				$('#food-display').prepend(foodDiv);
+				
+				
+					$('img').click(function(){
+						if($(this).attr('src')==still){
+							$(this).attr('src',animated);
+						}
+						else if($(this).attr('src')==animated){
+							$(this).attr('src',still);
+						}
+					})
+				
+			}
+	  })
+  })
+}
+
+function renderButtons(){
+	$('#foodBtn').empty();
+
+	for (var i=0; i<favfoods.length; i++){
+		var btn = $('<button>');
+		btn.addClass('food');
+		btn.attr('data-name', favfoods[i]);
+		btn.text(favfoods[i]);
+		$('#foodBtn').append(btn);
+	}
+}
+
+$('#add-food').on('click',function(event){
+	event.preventDefault();
+	var addfood = $('#food-input').val().trim();
+	favfoods.push(addfood);
+	renderButtons();
+	getGif();
+})
+
+
+
+
+
+
+
